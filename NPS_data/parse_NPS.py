99aa2deb167bb83fdb93183155198623539aed7e
Jamie Mancini <jamiemancini@gmail.com>
2022-07-02 13:11:02 -0700

    created py file to parse data

---
+++ NPS_data/parse_NPS.py
@@ -0,0 +1,42 @@
+import requests
+
+
+response = requests.get("https://developer.nps.gov/api/v1/campgrounds?stateCode=WA&limit=100&api_key=dUmx4agnRasxDjIEy7SjiPQhHQng1kou2aVwTD7b")
+data=response.json()["data"]
+
+
+for campground in data:
+    if campground["fees"]==[]:
+        print(f'{campground["name"]}: Fees is an empty list')
+    else:
+        print(campground["fees"][0]['cost'])
+
+# for campground in data:
+    if campground["amenities"]['cellPhoneReception']=="":
+        print(f'{campground["name"]}: cellPhoneReception is an empty string')
+    else:
+        print(campground["amenities"]['cellPhoneReception'])
+
+for campground in data:
+    if campground["amenities"]['toilets']==[]:
+        print(f'{campground["name"]}: toilets is an empty string')
+    else:
+        print(campground["amenities"]['toilets'][0])
+
+for campground in data:
+    if campground["numberOfSitesReservable"]=="":
+        print(f'{campground["name"]}: "numberOfSitesReservable" is an empty string')
+    else:
+        print(campground["numberOfSitesReservable"])
+
+for campground in data:
+    if campground["numberOfSitesFirstComeFirstServe"]=="":
+        print(f'{campground["name"]}: "numberOfSitesFirstComeFirstServe" is an empty string')
+    else:
+        if campground["numberOfSitesFirstComeFirstServe"]=="0":
+            print(f'{campground["name"]} is 0')
+
+
+
+
+
